39977bdc7bfa81093ee8f80b78b22444ce088858
Danil <tolmachdanil@gmail.com>
2021-09-08 00:43:07 +0300

    написал первый тест

---
+++ tests/test_bot.py
@@ -0,0 +1,15 @@
+from unittest.mock import patch
+
+
+def test_get_new_message(bot_fixture, ping_data):
+    with patch('requests.Session.request') as mock_get:
+        mock_get.return_value.json.return_value = ping_data
+
+        @bot_fixture.handler.message_new()
+        def response(message):
+            assert message.message.text == 'Hello'
+
+        # костыль для перехватывания ровно одного события
+        for update in bot_fixture._longpoll.run():
+            bot_fixture.handler.handle(update)
+            return
